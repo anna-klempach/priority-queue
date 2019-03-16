80b68236663c9bdb2b7330ab7a7e29f919dad1a5
anna-klempach <klempach.teaching@gmail.com>
2019-03-17 00:57:41 +0300

    feat: constructor; appendChild; removeChild; remove; swapWithParent

--- src/node.js
+++ src/node.js
@@ -1,23 +1,89 @@
 class Node {
-	constructor(data, priority) {
+    constructor(data, priority) {
+        this.data = data;
+        this.priority = priority;
 
-	}
+        this.parent = null;
+        this.left = null;
+        this.right = null;
+    }
 
-	appendChild(node) {
+    appendChild(node) {
+        node.parent = this;
+        if (!this.left) {
+            this.left = node;
+        } else {
+            if (!this.right) {
+                this.right = node;
+            }
+        }
+    }
 
-	}
+    removeChild(node) {
+        if (this.left == node) {
+            node.parent = null;
+            this.left = null;
+        } else {
+            if (this.right == node) {
+                node.parent = null;
+                this.right = null;
+            } else {
+                throw error;
+            }
+        }
+    }
 
-	removeChild(node) {
+    remove() {
+        if (this.parent) {
+            this.parent.removeChild(this);
+        }
+    }
 
-	}
+    swapWithParent() {
+        if (this.parent) {
+            let swapNode = new Node();
+            let grandParent = new Node();
+            grandParent = this.parent.parent;
 
-	remove() {
+            let parent = new Node();
+            parent = this.parent;
 
-	}
+            let child = new Node();
+            child = this;
 
-	swapWithParent() {
-		
-	}
+            if (parent == grandParent.left) {
+                grandParent.left = child;
+
+            } else {
+                grandParent.right = child;
+            }
+
+            child.parent = grandParent;
+            parent.parent = child;
+
+            if (child == parent.left) {
+                child.left = parent;
+                child.right = parent.right;
+
+            } else {
+                child.left = parent.left;
+                child.right = parent;
+            }
+            child.left.parent = this.parent;
+            child.right.parent = this.parent;
+            parent.left = child.left;
+            parent.right = child.right;
+
+            this.parent.parent.left = grandParent.left;
+            this.parent.parent.right = grandParent.right;
+            this.parent.left = parent.left;
+            this.parent.right = parent.right;
+            this.parent.parent = parent.parent;
+            this.left = child.left;
+            this.right = child.right;
+            this.parent = child.parent;
+        }
+    }
 }
 
 module.exports = Node;
