1e39f4e9a80c830d174b7c359c9159b3e54d5182
anna-klempach <klempach.teaching@gmail.com>
2019-03-17 22:43:05 +0300

    feat: constructor, push, insertNode in max-heap

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,14 +2,21 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		let parentNodes = [];
+		this.parentNodes = parentNodes;
 	}
 
 	push(data, priority) {
-		
+	let node = new Node(data, priority);
+	this.insertNode(node);
+	this.shiftNodeUp(node);
 	}
 
 	pop() {
+		if (this.root) {
+			this.removeChild(node);
+		}
 		
 	}
 
@@ -26,18 +33,38 @@ class MaxHeap {
 	}
 
 	isEmpty() {
-		
+		if (this.root == null && this.parentNodes.length == 0) {
+			return true;
+		}
+		return false;
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
-		
+		if (this.root == null && this.parentNodes.length == 0) {
+			this.root = node;
+			this.parentNodes.push(node);
+
+		} else {
+			this.parentNodes[0].appendChild(node);
+			this.parentNodes.push(node);
+	
+			if (this.parentNodes[0].left && this.parentNodes[0].right) {
+				this.parentNodes.shift();
+			}
+			
+		}
 	}
 
 	shiftNodeUp(node) {
+
+		
+			node.shiftWithParent();
+		
 		
 	}
 
