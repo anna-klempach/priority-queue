6456cd66b1a2f82c5eb39df9549566e706217e33
anna-klempach <klempach.teaching@gmail.com>
2019-03-21 01:30:43 +0300

    feat: detachRoot, size, isEmpty

--- src/max-heap.js
+++ src/max-heap.js
@@ -5,6 +5,8 @@ class MaxHeap {
 		this.root = null;
 		let parentNodes = [];
 		this.parentNodes = parentNodes;
+		let nodesArray = [];
+		this.nodesArray = nodesArray;
 	}
 
 	push(data, priority) {
@@ -16,11 +18,20 @@ class MaxHeap {
 	pop() {
 		if (this.root) {
 			this.removeChild(node);
+			this.nodesArray.splice(this.nodesArray.indexOf(node),1);
 		}
 		
 	}
 
 	detachRoot() {
+		let detachedRoot = new Node();
+		detachedRoot = this.root;
+		if (this.parentNodes.indexOf(detachedRoot) > -1){
+			this.parentNodes.shift();
+		}
+		this.root = null;
+		return detachedRoot;
+
 		
 	}
 
@@ -29,7 +40,8 @@ class MaxHeap {
 	}
 
 	size() {
-		
+		let size = this.nodesArray.length;
+		return size;
 	}
 
 	isEmpty() {
@@ -41,23 +53,27 @@ class MaxHeap {
 
 	clear() {
 		this.root = null;
-		this.parentNodes = [];
+		this.parentNodes.length = 0;
 	}
 
 	insertNode(node) {
+		
 		if (this.root == null && this.parentNodes.length == 0) {
 			this.root = node;
 			this.parentNodes.push(node);
+			this.nodesArray.push(node);
 
 		} else {
 			this.parentNodes[0].appendChild(node);
 			this.parentNodes.push(node);
+			this.nodesArray.push(node);
 	
 			if (this.parentNodes[0].left && this.parentNodes[0].right) {
 				this.parentNodes.shift();
 			}
 			
 		}
+
 	}
 
 	shiftNodeUp(node) {
@@ -95,7 +111,49 @@ class MaxHeap {
 
 	shiftNodeDown(node) {
 		
+		let indexParent = this.nodesArray.indexOf(node);
+		let swapArray = [];
+
+		let maxNode = new Node();
+		let maxIndex = indexParent;
+		let nodePriority = node.priority;
+		let maxPriority = nodePriority;
+		while (true) {
+			maxNode = node;
+			if (node.left && node.left.priority > maxPriority) {
+				maxNode = node.left;
+				maxPriority = node.left.priority;
+				maxIndex = this.nodesArray.indexOf(node.left);
+			}
+	
+			if (node.right && node.right.priority > maxPriority) {
+				maxNode = node.right;
+				maxPriority = node.left.priority;
+				maxIndex = this.nodesArray.indexOf(node.right);
+			}
+
+			swapArray.push(maxNode);
+
+			
+			if (maxNode !== node) {
+		
+				if (this.root === node) {
+					this.root = maxNode;
+				}
+				indexParent = maxIndex;
+				node = this.nodesArray.indexOf(maxIndex);
+				maxPriority = nodePriority;
+			} else break;
+		}
+
+		let swapArrayLength = swapArray.length;
+		
+			for (let i = 0; i < swapArrayLength-1; i++) {
+				node = swapArray[i];
+				node.swapWithParent();
+		}
 	}
-}
+}		
+
 
 module.exports = MaxHeap;
